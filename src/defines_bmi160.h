893b4872f8c243d4862c4485fbfc21a075e76026
unlogisch04 <98281608+unlogisch04@users.noreply.github.com>
2024-05-30 15:28:34 +0200

    Enable Mag and QMC on BMI160

--- src/defines_bmi160.h
+++ src/defines_bmi160.h
@@ -27,8 +27,8 @@
 // If only 1 out of 2 trackers has a mag, tracker without a mag should still function normally.
 // NOT USED if USE_6_AXIS == true
 // Pick one:
-#define BMI160_MAG_TYPE BMI160_MAG_TYPE_HMC
-// #define BMI160_MAG_TYPE BMI160_MAG_TYPE_QMC
+//#define BMI160_MAG_TYPE BMI160_MAG_TYPE_HMC
+#define BMI160_MAG_TYPE BMI160_MAG_TYPE_QMC
 
 // Use VQF instead of mahony sensor fusion.
 // Features: rest bias estimation, magnetic distortion rejection.
