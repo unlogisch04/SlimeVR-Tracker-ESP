9ad44d6c5fe782c7d1b805899693973814d334a1
unlogisch04 <98281608+unlogisch04@users.noreply.github.com>
2026-03-20 01:33:04 +0100

    disable regular serial output

--- src/debug.h
+++ src/debug.h
@@ -54,7 +54,7 @@
 #define serialDebug false  // Set to true to get Serial output for debugging
 #define serialBaudRate 115200
 #define LED_INTERVAL_STANDBY 10000
-#define PRINT_STATE_EVERY_MS 60000
+#define PRINT_STATE_EVERY_MS 0
 
 // Determines how often we sample and send data
 #define samplingRateInMillis 10
